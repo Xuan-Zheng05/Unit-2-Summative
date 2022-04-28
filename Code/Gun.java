242d605d881a159efb670b5d7520d7e60422adea
Xuan Zheng <eagledude200@gmail.com>
2022-04-28 12:25:45 -0400

    Added gun class & deleted placeholder

---
+++ Code/Gun.java
@@ -0,0 +1,12 @@
+/**
+ * Name: Xuan
+ * Date: 04/28/2022
+ *
+ * Description:
+ * A program that houses the Gun class and all its attributes, accessors,
+ * mutators, and other methods.
+ */
+
+public class Gun {
+
+}
