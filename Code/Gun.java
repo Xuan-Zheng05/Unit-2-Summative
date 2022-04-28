dc4f46b6d4d7213fce4a549f503ebfd2289b6ed1
Xuan Zheng <eagledude200@gmail.com>
2022-04-28 12:40:05 -0400

    Update Gun.java

--- Code/Gun.java
+++ Code/Gun.java
@@ -8,5 +8,49 @@
  */
 
 public class Gun {
+    /*
+     * Attributes
+     */
 
+    /** name of the gun */
+    private String name;
+
+    /** if the gun is a sidearm */
+    private boolean sidearm;
+
+    /** cost of the gun */
+    private int cost;
+
+    /** if the fire mode is automatic */
+    private boolean automatic;
+
+    /** time it takes for the gun to reload */
+    private double reloadSpeed;
+
+    /** how much ammo is in each magazine */
+    private int magazine;
+
+    /** how many bullets the gun has in reserve */
+    private int reserve;
+
+    /** amount of dmg each shot does to the head */
+    private int headDmg;
+
+    /** amound of dmg each shot does to the body */
+    private int bodyDmg;
+
+    /** amound of dmg each shot does to the leg */
+    private int legDmg;
+
+    /** how many bullets the gun fires each second */
+    private int fireRate;
+
+    /**
+     * Constructor
+     * 
+     * @param name      name of the gun
+     * @param sidearm   if the gun is a sidearm
+     * @param cost      cost of the gun
+     * @param automatic firemode of the gun
+     */
 }
