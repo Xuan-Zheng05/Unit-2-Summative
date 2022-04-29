60af212bfc67f98dece2a48487fd1dd3897aef12
Bonny C <70673137+xStylistic@users.noreply.github.com>
2022-04-28 23:53:52 -0400

    Update GunMain.java

--- Code/GunMain.java
+++ Code/GunMain.java
@@ -1,19 +1,21 @@
 
 /** 
-*   Name: Xuan
-*   Date: 04/28/2022
-*
-*   Description: 
-*   Main class demonstrating functionality of the Gun class
+* Name: Xuan
+* Date: 04/28/2022
+* Description: Main class demonstrating functionality of the Gun class
 */
 
 import java.util.*;
 import java.io.*;
 
 public class GunMain {
+    
     public static void main(String[] args) throws IOException {
+        
         Gun vandal = new Gun("Vandal", 2900, true, 2.5, 25, 25, 75, 9.75, 160, 40, 34);
         vandal.shoot(5);
         System.out.println(vandal.toString());
+     
     }
+    
 }
