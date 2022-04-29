cfe06e3408ddd10f46940eec808be9c40327f296
Xuan Zheng <eagledude200@gmail.com>
2022-04-29 14:01:49 -0400

    added space

--- Code/Sprint1Main.java
+++ Code/Sprint1Main.java
@@ -31,6 +31,7 @@ public class Sprint1Main {
         for (int i = 0; i < 4; i++) {
             System.out.println(abilities[i].getName());
         }
+        System.out.println();
         System.out.println(nauxzd.toString());
 
     }
