74797eabd15c983b6fdab769311a76e7da9b5249
Bonny C <70673137+xStylistic@users.noreply.github.com>
2022-04-29 00:22:21 -0400

    Update Ability.java

--- Code/Ability.java
+++ Code/Ability.java
@@ -1,8 +1,7 @@
 /**
- * Names: Bonny Chen
- * Date: April 28th, 2022
- * Description: The abilities class and all its attributes, accessors, and
- * mutator methods
+ * Names: Bonny Chen 
+ * Date: April 28th, 2022 
+ * Description: The abilities class and all its attributes, accessors, and mutator methods
  */
 
 public class Ability {
@@ -19,6 +18,9 @@ public class Ability {
 
 	/** amount of damage the ability does */
 	private int damage;
+	
+	/** how many of the ability the agent has */
+	private int amount;
 
 	/** if the ability blinds */
 	private boolean blind;
@@ -41,42 +43,117 @@ public class Ability {
 	/**
 	 * Constructor to create an ability
 	 * 
-	 * @param name:       name of the ability
-	 * @param cost:       cost of the ability
-	 * @param damage:     how much damage the ability does
-	 * @param blind:      if it blinds
-	 * @param slow:       if it slows
-	 * @param concuss:    if it concusses
-	 * @param decay:      if it decays
-	 * @param vulnerable: if it can make enemies vulnerable
-	 * @param detect:     if it can detects enemies
+	 * @param name: 		name of the ability 
+	 * @param cost: 		cost of the ability 
+	 * @param damage: 		how much damage the ability does
+	 * @param amount:  how many of the ability the agent has available to use
+	 * @param blind: 		if it blinds
+	 * @param slow: 		if it slows
+	 * @param concuss: 		if it concusses
+	 * @param decay: 		if it decays
+	 * @param vulnerable: 	if it can make enemies vulnerable
+	 * @param detect: 		if it can detects enemies
 	 */
-	public Ability(String name, int cost, int damage, boolean blind, boolean slow, boolean concuss, boolean decay,
-			boolean vulnerable, boolean detect) {
-
-		this.name = name;
-		this.cost = cost;
-		this.damage = damage;
-		this.blind = blind;
-		this.slow = slow;
-		this.concuss = concuss;
-		this.decay = decay;
-		this.vulnerable = vulnerable;
-		this.detect = detect;
-
-	}
+	public Ability(String name, int cost, int damage, int amount, boolean blind, boolean slow, boolean concuss, boolean decay, boolean vulnerable, boolean detect) { 
+		
+        this.name = name;
+        this.cost = cost; 
+        this.damage = damage; 
+        this.amount = amount;
+        this.blind = blind; 
+        this.slow = slow;
+        this.concuss = concuss; 
+        this.decay = decay; 
+        this.vulnerable = vulnerable;
+        this.detect = detect; 
+        
+    }
 
 	/*
-	 * Accessors
+	 * Accessor Methods
 	 */
-
-	public String getName() {
-		return this.name;
+	
+	public String name() { 
+		
+		return this.name; 
+		
+	} 
+	
+	public int cost() { 
+		
+		return this.cost(); 
+		
 	}
-
+	
+	public int damage() { 
+		
+		return this.damage();
+		
+	}
+	
+	public int amount() { 
+		
+		return this.amount();
+		
+	}
+	
+	public boolean blind() { 
+		
+		return this.blind;
+		
+	}
+	
+	public boolean slow() { 
+		
+		return this.slow;
+		
+	}
+	
+	public boolean concuss() { 
+		
+		return this.concuss;
+		
+	}
+	
+	public boolean decay() { 
+		
+		return this.decay;
+		
+	}
+	
+	public boolean vulnerable() { 
+		
+		return this.vulnerable;
+		
+	}
+	
+	public boolean detect() { 
+		
+		return this.detect;
+		
+	}
+	
 	/*
 	 * Methods
 	 */
+	
+	/**
+	 * Method to cast an ability 
+	 * 
+	 * @param times: the number of times the ability is casted
+	 */
+    public void cast(int times) {
+
+        this.amount -= times;
+
+        if (this.amount < 0) {
+
+            this.amount = 0;
+
+        }
+
+    }
+  
 
 	/**
 	 * Displays all the attributes of the ability in a String
@@ -90,6 +167,7 @@ public class Ability {
 		abilityString = abilityString + "Name: " + this.name + "\n";
 		abilityString = abilityString + "Cost: $" + this.cost + "\n";
 		abilityString = abilityString + "Damage: " + this.damage + "\n";
+		abilityString = abilityString + "Amount: " + this.amount + "\n";
 		abilityString = abilityString + "Blinds: " + this.blind + "\n";
 		abilityString = abilityString + "Slows: " + this.slow + "\n";
 		abilityString = abilityString + "Concusses: " + this.concuss + "\n";
