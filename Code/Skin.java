a1208849d8adb9f694037a1265ce522de4f961ca
Yuting-Hou <99358635+Yuting-Hou@users.noreply.github.com>
2022-04-29 13:45:12 -0400

    Add files via upload
    Skin.java has no methods

---
+++ Code/Skin.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Yuting
+ *
+ */
+public class Skin {
+    
+    /** The name of the skin*/
+    String name;
+    
+    /** The valorant point cost of the skin*/
+    int cost;
+    
+    /** The texture of the skin (Not implement)*/
+    String texture;
+    
+}
---
+++ Code/UserProfile.java
@@ -0,0 +1,101 @@
+/**
+ * This is the player profile class, contains information about the player
+ * @author Yuting Hou
+ * 4/28/2022
+ */
+
+import java.util.ArrayList;
+
+public class UserProfile {
+
+    /** The name of the player*/
+    private String username;
+    
+    /** PlayerProfiles which is friends with this player */
+    ArrayList<UserProfile> friends; //This is an arraylist which just mean that the array is resizable
+    
+    //constructor
+    
+    /**
+     * Default constructor
+     */
+    public UserProfile() {
+	this.username = "";
+	this.friends = new ArrayList<UserProfile>();
+    }
+    
+    /**
+     * Constructor with username
+     * @param username the name of the user
+     */
+    public UserProfile(String username) {
+	if(username.length()<3) {
+	    System.out.println("Name cannot be smaller than 3 characters.");
+	}else if (username.length()>16) {
+	    System.out.println("Name cannot be bigger than 16 characters.");
+	}else {
+	    this.username = username;
+	}
+    }
+    
+    //methods
+    
+    /**
+     * This method searches for a friend
+     * @param friendName the name of the friend to be searched
+     * @return friend the profile of the friend
+     */
+    public UserProfile getFriend(String friendName) {
+	String curr;
+	for (int i = 0; i < friends.size(); i++) {
+	    curr = this.friends.get(i).getUsername();
+	    if(curr.equals(friendName)) {
+		return this.friends.get(i);
+	    }
+	}
+	System.out.println("No player found");
+	return null;
+    }
+    
+    public void addFriend(String friendName) {
+	
+    }
+    
+    
+    
+    //getters
+    
+    /**
+     * This method returns the user name of the player
+     * @return username the name of the player
+     */
+    public String getUsername() {
+	return this.username;
+    }
+    
+    /**
+     * This method return the user's friendlist
+     * @return friends the user's list of friends
+     */
+    public ArrayList<UserProfile> getFriends(){
+	return this.friends;
+    }
+    
+    //setters
+    
+    /**
+     * This method sets the username for the user
+     * Username cannot be smaller than 3 or bigger than 6 characters
+     * @param username the username to be changed into
+     */
+    public void setUsername(String username) {
+	if(username.length()<3) {
+	    System.out.println("Name cannot be smaller than 3 characters.");
+	}else if (username.length()>16) {
+	    System.out.println("Name cannot be bigger than 16 characters.");
+	}else {
+	    this.username = username;
+	}
+    }
+    
+}
