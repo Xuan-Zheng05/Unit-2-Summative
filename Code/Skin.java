9db1a17cd2de8dbdf13ec4d9b5b9a5b1f8aeb06e
Xuan Zheng <eagledude200@gmail.com>
2022-05-02 12:46:13 -0400

    Update Skin.java

--- Code/Skin.java
+++ Code/Skin.java
@@ -5,4 +5,31 @@
 
 public class Skin {
 
+    private String name;
+
+    private Gun gun;
+
+    private boolean upgrade;
+
+    private int price;
+
+    public Skin(String name, Gun gun, boolean upgrade, int price) {
+        this.name = name;
+        this.gun = gun;
+        this.upgrade = upgrade;
+        this.price = price;
+    }
+
+    public String toString() {
+
+        String skinString = "";
+
+        skinString = skinString + "Name: " + this.name + "\n";
+        skinString = skinString + "Gun: " + this.gun.getName() + "\n";
+        skinString = skinString + "Upgradeable: " + this.upgrade + "\n";
+        skinString = skinString + "Price: " + this.price + "VP\n";
+        return skinString;
+
+    }
+
 }
