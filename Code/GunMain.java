cad8ebf2648eff17b3bc0f78edf9a4a01bc65431
Xuan Zheng <eagledude200@gmail.com>
2022-05-02 13:10:17 -0400

    Update GunMain.java

--- Code/GunMain.java
+++ Code/GunMain.java
@@ -12,9 +12,14 @@ public class GunMain {
 
     public static void main(String[] args) throws IOException {
 
+        // initalized gun
         Gun vandal = new Gun("Vandal", 2900, true, 2.5, 25, 25, 75, 9.75, 160, 40, 34);
-        vandal.shoot(5);
+
+        // print out toString method
         System.out.println(vandal.toString());
+
+        // shoot gun 5 times and see new ammo
+        vandal.shoot(5);
 
     }
 
