6ea2c60946872a8d2104e0522cdd0b5b2b557081
Xuan Zheng <eagledude200@gmail.com>
2022-05-03 14:35:51 -0400

    changed "DeathMatch" to "Deathmatch"

--- Code/GameModeMain.java
+++ Code/GameModeMain.java
@@ -1,3 +1,4 @@
+
 /**
  * Names: Yifei Qi
  * Date: May.2th, 2022
@@ -8,22 +9,20 @@
 import java.util.*;
 import java.io.*;
 
-public class GameModeMain
-{
+public class GameModeMain {
     static private GameMode[] gameModeList = new GameMode[4];
-    public static GameMode[] initialGameModes ()
-    {
-        GameMode unrated = new GameMode("Unrated",40,25,10,false,true,false);
-        GameMode competitive = new GameMode("Competitive",40,25,10,false,true,true);
-        GameMode spikeRush = new GameMode("SpikeRush",15,7,10,true,false,false);
-        GameMode deathMatch = new GameMode ("DeathMatch",9,1,12,false,false,false);
-        GameMode shootingTest = new GameMode("ShootingTest",100000,1,100000,false,false,false);
-        GameMode[] gameModeList = {unrated,competitive,spikeRush,deathMatch,shootingTest};
+
+    public static GameMode[] initialGameModes() {
+        GameMode unrated = new GameMode("Unrated", 40, 25, 10, false, true, false);
+        GameMode competitive = new GameMode("Competitive", 40, 25, 10, false, true, true);
+        GameMode spikeRush = new GameMode("SpikeRush", 15, 7, 10, true, false, false);
+        GameMode deathMatch = new GameMode("Deathmatch", 9, 1, 12, false, false, false);
+        GameMode shootingTest = new GameMode("ShootingTest", 100000, 1, 100000, false, false, false);
+        GameMode[] gameModeList = { unrated, competitive, spikeRush, deathMatch, shootingTest };
         return gameModeList;
     }
 
-    public static GameMode[] getGameModeArray()
-    {
+    public static GameMode[] getGameModeArray() {
         return gameModeList.clone();
     }
 }
