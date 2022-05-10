07537273e09c9d91572553af6e1739e99fe566ea
Xuan Zheng <eagledude200@gmail.com>
2022-05-10 12:54:48 -0400

    finished sprint3main

--- Code/Sprint3Main.java
+++ Code/Sprint3Main.java
@@ -89,8 +89,8 @@ public class Sprint3Main {
         // neon shoots gun 3 times
         neon.getGun().shoot(6);
         // hit 3 times
-        viper.bulletHit(neon.getGun(), neon, 3);
-        System.out.println("Viper's current hp is" + viper.getAgentHp());
+        viper.bulletHit(neon.getGun(), neon, 2);
+        System.out.println("Viper's current hp is " + viper.getAgentHp());
 
         viper.getGun().shoot(10);
         neon.bulletHit(viper.getGun(), viper, 5);
