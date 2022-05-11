dfb0e8383d6e53721e3519da818ab181c1928563
Bonny C <70673137+xStylistic@users.noreply.github.com>
2022-05-11 13:55:58 -0400

    Update GameModeMain.java

--- Code/GameModeMain.java
+++ Code/GameModeMain.java
@@ -1,6 +1,5 @@
-
 /**
- * Names: Yifei Qi
+ * Names: Yifei Qi, Bonny Chen
  * Date: May.2th, 2022
  * Description: This program will initilzie the five gamemode in valorant (Unrated, Competitive, Spike rush, DeathMatch, Shooting test)
  * 
@@ -10,9 +9,9 @@ import java.util.*;
 import java.io.*;
 
 public class GameModeMain {
-
+    
     public static GameMode[] initialGameModes() {
-
+        
         GameMode unrated = new GameMode("Unrated", 25, 10, false, true, false, true);
         GameMode competitive = new GameMode("Competitive", 25, 10, false, true, true, true);
         GameMode spikeRush = new GameMode("SpikeRush", 7, 10, true, false, false, false);
@@ -20,9 +19,30 @@ public class GameModeMain {
         GameMode shootingTest = new GameMode("ShootingTest", 1, 100000, false, false, false, true);
         GameMode[] gameModeList = { unrated, competitive, spikeRush, deathMatch, shootingTest };
         return gameModeList;
-    }
+
+       }
 
     public static void main(String[] args) throws IOException {
+        
+        // initalized GameModes
+        GameMode[] gameModeList = GameModeMain.initialGameModes();
+        String s = gameModeList[(int) Math.floor(Math.random() * 5)].getName();
+        System.out.println("The gamemode is " + s);
+
+        if (s.equals("deathMatch")) { 
+
+            Ability.disableAbilities();
+            System.out.println("Abilities are disabled.");
+
+        }
+
+        if (s.equals("spikeRush")) { 
+
+            Gun.disableBuyGun();
+            System.out.println("You cannot purchase a gun.");
+
+        }
 
     }
+
 }
